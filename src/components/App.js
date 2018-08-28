88b48adf0db2696c4dac9827e36a1ec990f5e5b7
Roman Alexeychenko <roman.alexeychenko@gmail.com>
2018-08-28 18:52:02 +0300

    add pagination

--- src/components/App.js
+++ src/components/App.js
@@ -8,15 +8,15 @@ class App extends Component {
     }
 
     componentDidMount() {
-        this.apiGetNews()
+        this.apiGetNews(1, 10)
     }
 
-    apiGetNews = () => {
+    apiGetNews = (pageNumber, pageSize) => {
         console.log('---', 'get news')
-        this.setState({loading: true})
-        fetch('https://content.guardianapis.com/search?order-by=newest&page=1&page-size=10&api-key=9f756fb3-eb7e-4c78-b09e-fe352ae2620d')
+        this.setState({loading: true, page: pageNumber})
+        fetch(`https://content.guardianapis.com/search?order-by=newest&page=${pageNumber}&page-size=${pageSize}&api-key=9f756fb3-eb7e-4c78-b09e-fe352ae2620d`)
             .then(res => res.json())
-            .then(data => this.setState({news: data.response.results, loading: false}))
+            .then(data => this.setState({news: data.response.results, loading: false, totalPages: data.response.pages}))
             .catch(e => this.setState({loading: 'ERROR'}))
     }
 
@@ -31,13 +31,32 @@ class App extends Component {
                         return <li key = {index}>{name.webTitle}</li>
                     })}
                 </ul>
+                <div>
+                    <button onClick={this.prevPage}>Prev</button>
+                        <span>{this.state.page} of {this.state.totalPages}</span>
+                    <button onClick={this.nextPage}>Next</button>
+                </div>
             </div>
         )
     }
 
     refresh = () => {
-        this.apiGetNews()
+        this.apiGetNews(1, 10)
         console.log('---', 'refresh')
+    }
+
+    prevPage = () => {
+        let a = this.state.page - 1; // тут надо как то сделать проверку на то если a == 1 то не посылать запрос
+        this.setState({page: a});
+        this.apiGetNews(a, 10);
+        console.log('---', 'Prev Page')
+    }
+
+    nextPage = () => {
+        let a = this.state.page + 1;
+        this.setState({page: a});
+        this.apiGetNews(a, 10);
+        console.log('---', 'Next Page')
     }
 
 }
