3e53ec99e3368fe738831cced2429ea037e538fa
Roman Alexeychenko <roman.alexeychenko@gmail.com>
2018-08-28 17:26:20 +0300

    add button refresh

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,7 @@ class App extends Component {
     }
 
     apiGetNews = () => {
+        console.log('---', 'get news')
         this.setState({loading: true})
         fetch('https://content.guardianapis.com/search?order-by=newest&page=1&page-size=10&api-key=9f756fb3-eb7e-4c78-b09e-fe352ae2620d')
             .then(res => res.json())
@@ -19,12 +20,12 @@ class App extends Component {
             .catch(e => this.setState({loading: 'ERROR'}))
     }
 
-
     render() {
 
         return (
             <div>
                 {<h1>The Guardians News</h1>}
+                <button onClick={this.refresh}>Refresh</button>
                 <ul>
                     {this.state.news.length >= 1 && this.state.news.map(function(name, index) {
                         return <li key = {index}>{name.webTitle}</li>
@@ -32,6 +33,11 @@ class App extends Component {
                 </ul>
             </div>
         )
+    }
+
+    refresh = () => {
+        this.apiGetNews()
+        console.log('---', 'refresh')
     }
 
 }
